da89b57e8b018951f0a083eafc836925159027ee
Walter Tamboer <walter.tamboer@live.com>
2013-12-12 23:03:17 +0100

    Added a redirect to GitHub from the source subdomain

--- htdocs/source/index.php
+++ htdocs/source/index.php
@@ -6,14 +6,5 @@
  * @link https://github.com/pixelpolishers/pixelpolishers.com for the canonical source repository
  */
 
-error_reporting(E_ALL);
-ini_set('display_errors', true);
-ini_set('display_startup_errors', true);
-
-chdir(dirname(dirname(__DIR__)));
-
-// Setup autoloading
-include 'vendor/autoload.php';
-
-// Run the application!
-Zend\Mvc\Application::init(include 'config/application.config.php')->run()->send();
+header('Location:https://github.com/pixelpolishers');
+exit;
