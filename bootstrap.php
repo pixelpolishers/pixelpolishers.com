63a7858398de00911f89f14250d1811ee52e50ee
Walter Tamboer <walter.tamboer@live.com>
2013-12-14 12:01:41 +0100

    Added some sanity checks

--- bootstrap.php
+++ bootstrap.php
@@ -6,16 +6,27 @@
  * @link https://github.com/pixelpolishers/pixelpolishers.com for the canonical source repository
  */
 
-error_reporting(E_ALL);
-ini_set('display_errors', true);
-ini_set('display_startup_errors', true);
+// Set the extension value, this is used in the routes and makes debugging easier:
+$GLOBALS['extension'] = substr($_SERVER['HTTP_HOST'], strrpos($_SERVER['HTTP_HOST'], '.') + 1);
 
+// Enable PHP errors when we're debugging:
+if ($GLOBALS['extension'] == 'local') {
+    error_reporting(E_ALL);
+    ini_set('display_errors', true);
+    ini_set('display_startup_errors', true);
+}
+
+// Change the current working direction:
 chdir(__DIR__);
 
-// Setup autoloading
+// Setup autoloading:
 include 'vendor/autoload.php';
 
-$GLOBALS['extension'] = substr($_SERVER['HTTP_HOST'], strrpos($_SERVER['HTTP_HOST'], '.') + 1);
+// Make sure we have all the needed extensions:
+if (!extension_loaded('intl')) {
+    echo 'Failed to load the website. We need the intl PHP extension.';
+    exit;
+}
 
 // Run the application!
 Zend\Mvc\Application::init(include 'config/application.config.php')->run()->send();
