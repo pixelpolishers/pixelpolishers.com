b9812c69d0615897251ae243dfa553d5dbdb7de1
Walter Tamboer <walter.tamboer@live.com>
2013-12-15 19:11:18 +0100

    Added the verbose flag to composer

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ git reset --hard HEAD
 git pull origin
 
 # Retrieve the latest dependencies:
-php composer.phar self-update
-php composer.phar update --no-interaction --no-dev
+php composer.phar self-update --verbose
+php composer.phar update --no-interaction --no-dev --verbose
 
 # Make sure that all the needed files are executable:
 chmod 0777 build.sh
