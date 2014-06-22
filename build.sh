b9c1947809b9febc11fddcda6d4f75330238823e
Walter Tamboer <walter.tamboer@live.com>
2014-06-22 13:12:33 +0200

    Made the build file more verbose

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ git reset --hard HEAD
 git pull origin
 
 # Retrieve the latest dependencies:
-php composer.phar self-update --verbose
-php composer.phar update --no-interaction --no-dev --verbose
+php composer.phar self-update -vvv
+php composer.phar update --no-interaction --no-dev -vvv
 
 # Make sure that all the needed files are executable:
 chmod 0777 build.sh
